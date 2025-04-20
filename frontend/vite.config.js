bc9c639f86607bc651ffec7bc135371055274284
Jiří Smolák <jirysmolak@gmail.com>
2025-04-20 21:55:09 +0200

    Úprava Dockerfile pro instalaci specifických verzí Vite a pluginu React a vytvoření minimální konfigurace vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react');
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
 
 module.exports = defineConfig({
   plugins: [react()],
   server: {
     port: 3000
   }
-});
+})
